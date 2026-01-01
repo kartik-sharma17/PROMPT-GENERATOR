a1ca79ce531bf1cfaa5c5ca11d96c2991deda5d6
Kartik Sharma <kartiksharma88103@gmail.com>
2026-01-01 23:13:24 +0530

    completed login ui

--- apps/web/@core/customInput.tsx
+++ apps/web/@core/customInput.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/ui/input"
 
 export const CustomInput = (props: any) => {
-    const { placeholder, label, formik } = props
+    const { placeholder, label, formik, name, required = true, customClass } = props
     return (
-        <div className="flex flex-col items-start">
-            {label && (<label className="text-[#929294]">{label}</label>)}
-            <Input placeholder={placeholder} />
+        <div className="flex flex-col items-start mb-4">
+            {label && (<label className="text-[#929294] text-sm ml-1! mb-1!">{label} {required && (<span className="text-red-600"> *</span>)}</label>)}
+            <Input name={name} className={`text-white focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none px-3! py-5! rounded-lg! bg-[#1C1D21] border-transparent ${formik.errors[name] && ("border-red-600")} ${customClass && (customClass)}`} onChange={formik.handleChange} value={formik.values[name]} placeholder={placeholder} />
         </div>
     )
 }
--- apps/web/@core/index.ts
+++ apps/web/@core/index.ts
@@ -1,1 +1,2 @@
 export * from './customInput'
+export * from './customPasswordInput'
