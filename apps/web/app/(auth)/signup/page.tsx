e1861458a60892f273f53dc6a88cf83afbd9839e
Kartik Sharma <kartiksharma88103@gmail.com>
2026-01-02 21:14:07 +0530

    completed signup page

--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -1,8 +1,68 @@
-import React from 'react'
+"use client"
+
+import { CustomInput, CustomPasswordInput } from "@/@core"
+import { registerSchema } from "@/zodSchema"
+import { useFormik } from "formik"
+import Link from "next/link"
+import { toFormikValidationSchema } from "zod-formik-adapter"
+
+const initialValues = {
+  full_name: "",
+  email: "",
+  phone: "",
+  password: ""
+}
 
 const page = () => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema: toFormikValidationSchema(registerSchema),
+    onSubmit: (values) => {
+      console.log("this si a submitting")
+      console.log(values)
+    }
+  })
+
   return (
-    <div>This is signup page</div>
+    <div className="h-screen overflow-hidden relative">
+      <img
+        src="/assets/img/loginBG.jpg"
+        alt="Login Background"
+        className="h-auto w-screen object-contain"
+      />
+      <div className="absolute h-full w-full top-0 flex flex-col justify-center items-center">
+        <h1 className="mb-7 text-5xl font-bold bg-linear-to-r from-emerald-400 to-cyan-500 bg-clip-text text-transparent">
+          Prompt Smarter. Create Faster.
+        </h1>
+
+        <div className="bg-(--bg-main) border-white rounded-(--custom-radius) p-10 border-rou text-center">
+          <form onSubmit={formik.handleSubmit}>
+            <h3 className="text-white text-2xl mb-2">Create Your Account</h3>
+            <h6 className="text-[#929294] text-sm mb-7"> Create an account and start your journey with us</h6>
+            <CustomInput customClass="w-85" name="full_name" placeholder="Enter Your Full Name" formik={formik} label="Full Name" />
+
+            <CustomInput customClass="w-85" name="email" placeholder="Enter Your Email" formik={formik} label="Email" />
+
+            <CustomInput customClass="w-85" name="phone" placeholder="Enter Your Phone" formik={formik} label="Phone No." />
+
+            <CustomPasswordInput customClass="w-85" name="password" placeholder="Create Your Password" formik={formik} label="Password" />
+
+            <button type="submit" className="text-white mt-6 bg-linear-to-r from-emerald-400 to-cyan-500 w-full p-2.5 rounded-md text-sm">Register</button>
+
+            <hr className="border-[#6b6b6b] mt-10" />
+            <p className="text-[#929294] text-xs mt-5">
+              Already have an account ?{" "}
+              <Link href="/login" className="text-emerald-300! font-medium hover:underline!">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+      {/* <div className="absolute top-0">
+        <img src="/assets/img/logo.webp" className="h-auto w-20" alt="" />
+      </div> */}
+    </div>
   )
 }
 
---
+++ apps/web/zodSchema/resgiterSchema.tsx
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  full_name: z
+    .string()
+    .min(2, "Full name must be at least 2 characters"),
+
+  email: z
+    .string()
+    .email("Invalid email address"),
+
+  phone: z
+    .string()
+    .regex(/^[0-9]{10}$/, "Phone number must be 10 digits"),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters"),
+});
