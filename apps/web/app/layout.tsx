db015fc66ab8303455c8875eb32d1b180a25cef7
Kartik Sharma <kartiksharma88103@gmail.com>
2026-05-08 07:48:22 +0530

    added google and microsoft user tracking tools

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import './globals.css'
 import { ReduxProvider } from "@/reduxConfig/reduxProvider";
+import GoogleAnalytics from "@/@comp/GoogleAnalytics";
+import MicrosoftClarity from "@/@comp/MicrosoftClarity";
 
 export const metadata: Metadata = {
   title: "Prompt Generator",
@@ -20,6 +22,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Passero+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <GoogleAnalytics/>
+        <MicrosoftClarity/>
+
         <ReduxProvider>
           {children}
         </ReduxProvider>
